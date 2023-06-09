c74a5b2dd301245530a2f85a67eed6e7e3f7e8dc
Elshadai Hailu <elshadaihailu@Elshadais-iMac.local>
2023-06-09 14:45:14 -0700

    check if the array of numbers contains '1' and if it does, it will replace it with 'beep'

--- js/script.js
+++ js/script.js
@@ -1,7 +1,29 @@
+
+
 function beepBoop(userInput) {
     let newArray = [];
-    for (i = 0; i <= userInput; i++){
+    for (i = 0; i <= userInput; i++) {
         newArray.push(i);
     }
+    console.log('counting up works');
+    newArray.forEach(function (element) {
+        // const oneLocation = newArray.indexOf(1);
+        newArray[1] = "beep!"
+    })
     return newArray;
-}
+    }
+
+//     newArray.forEach(function (element) {
+//         if (newArray.includes(element)) {
+//             const oneLocation = newArray.indexOf(element);
+//             newArray[oneLocation] = "beep";
+//         }
+//     })
+// }
+
+// function replace(userInput)
+// if (newArray(userInput).includes(1)) {
+//     const one = newArray.indexOf(element);
+//     let newNum = element.replace(newArray[one], "beep");
+
+// }
