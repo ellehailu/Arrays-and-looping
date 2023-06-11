f04abf04d23d5e96bcd5e2aa51d74ad950e4c7cc
Elshadai Hailu <elshadaihailu@Elshadais-iMac.local>
2023-06-10 20:49:36 -0700

    create a function to handle the user input

--- README.md
+++ README.md
@@ -27,21 +27,3 @@ Expected output: [0, 'beep!', 'boop!', 3, 4, 5, 6, 7, 8, 9, 'beep!', 'beep!', 'b
 Test: "it should return an array of numbers 0 to the user's inputteed number with number 3 replaced with the text 'won't you be my neighbor'";
 Code: beepBoop(35)
 Expected output:[0, 'beep!', 'boop!', "Won't you be my neighbor?", 4, 5, 6, 7, 8, 9, 'beep!', 'beep!', 'beep!', 'beep!', 'beep!', 'beep!', 'beep!', 'beep!', 'beep!', 'beep!', 'boop!', 'boop!', 'boop!', 'boop!', 'boop!', 'boop!', 'boop!', 'boop!', 'boop!', 'boop!', "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?", "Won't you be my neighbor?"]
-
-Instructions
-
-//Identify if the user input includes 1
-
-//Logic
-
-// 1. create an array of the user's input
-// 2. Sort through the array to identify if the user's inputted number includes 1, 2, or 3
-
-// Create conditions for replacement for each number
-// 1 = "Beep!"
-// 2 = "Boop!"
-// 3 = "Won't you be my neightbor?"
-
-//return a list of numbers starting at zero and ending at the user's inputed number
-
-//reset form for the user so the user can run through the form multiple times
--- js/script.js
+++ js/script.js
@@ -2,22 +2,36 @@
 
 function beepBoop(userInput) {
     let newArray = [];
-    for (i = 0; i <= userInput; i++) {
+    for (let i = 0; i <= userInput; i++) {
         newArray.push(i);
     }
-    console.log('counting up works');
-    newArray.forEach(function(element) {
-        if (newArray[element].toString().includes('3')) {
-            newArray[element] = "Won't you be my neighbor?";
-        }
-        else if (newArray[element].toString().includes('2')) {
-            newArray[element] = "Boop!";
-        }
-        else if (newArray[element].toString().includes('1')) {
-            newArray[element] = "Beep!";
-        }
-    })
-    return newArray;
-}
+        console.log('counting up works');
+        newArray.forEach(function(element) {
+            if (newArray[element].toString().includes('3')) {
+                newArray[element] = "Won't you be my neighbor?";
+            }
+            else if (newArray[element].toString().includes('2')) {
+                newArray[element] = "Boop!";
+            }
+            else if (newArray[element].toString().includes('1')) {
+                newArray[element] = "Beep!";
+            }
+        })
+        return newArray;
+    }
 
-//UI logic 
+    //UI logic 
+function handleForm(event) {
+    event.preventDefault();
+    const userInput = document.getElementById("numberInput").value;
+    const updatedNums = beepBoop(userInput);
+    let result = document.createElement("p")
+    result.innerText = updatedNums;
+    document.body.append(result);
+    console.log(result);
+}
+    window.addEventListener("load", function () {
+        const form = document.getElementById("number-form");
+        form.addEventListener("submit", handleForm);
+    });
+
